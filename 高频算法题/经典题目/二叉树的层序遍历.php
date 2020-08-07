4f91313442a289e4db339b2eb112b03aaa447d26
tengyunlong <yunlongteng@sfmail.sf-express.com>
2020-08-07 21:00:05 +0800

    feat: 增加题目: 二叉树的层序遍历

---
+++ 高频算法题/经典题目/二叉树的层序遍历.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/leetbook/read/2020-top-interview-questions/xogl61/
+ *
+ * 给你一个二叉树，请你返回其按 层序遍历 得到的节点值。 （即逐层地，从左到右访问所有节点）。
+ *
+ *  
+ *
+ * 示例：
+ * 二叉树：[3,9,20,null,null,15,7],
+ *
+ *    3
+ *   / \
+ *  9  20
+ *  /  \
+ * 15   7
+ * 返回其层次遍历结果：
+ *
+ * [
+ * [3],
+ * [9,20],
+ * [15,7]
+ * ]
+ *
+ */
+
+require_once "../../数据结构/TreeNode.php";
+
+class Solution
+{
+
+    /**
+     * @param TreeNode $root
+     * @return Integer[][]
+     */
+    private $result = [];
+
+    function levelOrder($root)
+    {
+        $this->deepTrace($root, 0);
+        return $this->result;
+    }
+
+    // 深度遍历
+    function deepTrace($rootNode, $level)
+    {
+        if ($rootNode == null) {
+            return;
+        }
+        $this->result[$level][] = $rootNode->val;
+        $this->deepTrace($rootNode->left, $level + 1);
+        $this->deepTrace($rootNode->right, $level + 1);
+    }
+
+    // 广度优先
+}
+
+mock();
+
+function mock()
+{
+    echo "======= test case start =======\n";
+
+    $rootNode = new TreeNode(3);
+    $node1 = new TreeNode(9);
+    $node2 = new TreeNode(20);
+
+    $node3 = new TreeNode(15);
+    $node4 = new TreeNode(7);
+
+    $rootNode->left = $node1;
+    $rootNode->right = $node2;
+    $node1->left = $node3;;
+    $node2->right = $node4;
+
+    $ret = (new Solution())->levelOrder($rootNode);
+    var_dump($ret);
+    echo "======= test case end =======\n";
+}
